f2b55d6cf178be993f8679df82b7386859f51447
Ryan <ryanjobs234@gmail.com>
2025-04-23 21:45:02 -0300

    Vamos recomeçar com está base do professor

--- api/index.js
+++ api/index.js
@@ -1,19 +1,23 @@
-import express from 'express';
-import cors from 'cors';
-import {connectToDatabase } from './config/database.js';
-import productsRoutes from './routes/productsRoutes.js';
+import express from 'express'
+import cors from 'cors' // Importa o módulo cors
+import { connectToDatabase } from './config/db.js'
+import municipiosRoutes from './routes/municipios.js'
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
 
-app.use(cors());
-app.use(express.json());
-app.use('/', express.static('public'));
-app.use('/api/products', productsRoutes);
-app.use('/img', express.static('public/img/img-storage.png'));
 
+app.use(cors()) //Habilita o CORS Cross-Origin resource sharing
+app.use(express.json())//parse do JSON
+//rota pública
+app.use('/', express.static('public'))
+//Rotas do app
+app.use('/api/municipios', municipiosRoutes)
+//define o favicon
+app.use('/favicon.ico', express.static('public/images/logo.png'))
+//start the server
 connectToDatabase(app).then(() => {
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-});
+        console.log(`Servidor rodando na porta ${PORT}!`)
+    })
+})
