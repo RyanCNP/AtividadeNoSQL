41089272c752091fea968a6ee6c6a5d828d7e845
Ryan <ryanjobs234@gmail.com>
2025-04-27 09:25:04 -0300

    Aprimoramento da Validação => Query Parms

--- api/middleware/storage_validation.js
+++ api/middleware/storage_validation.js
@@ -25,17 +25,29 @@ export const validatetStorage = [
         .isLength({ max: 10 })
         .withMessage("O código do produto deve ter no máximo 10 caracteres.")
         .matches(/^[A-Z]{2}-[A-Z]{3}-\d{3}$/)
-        .withMessage("Formato inválido. Use o padrão: AA-AAA-001."),
+        .withMessage("Formato inválido. Use o padrão: AA-AAA-001.")
+        .custom(async (cod_prod, { req }) => {
+            const db = req.app.locals.db;
+            const query = { cod_prod };
+            if (req.method === "PUT" && req.params.id) {
+                query["_id"] = { $ne: new ObjectId(req.params.id) };
+            }
+            const existe = await db.collection("storage").countDocuments(query);
+            if (existe > 0) {
+                throw new Error("O produto informado ja está cadastrado no estoque");
+            }
+            return true;
+        }),
     check("descricao")
         .notEmpty()
         .withMessage("A descrição é obrigatória.")
         .isLength({ max: 200 })
         .withMessage("A descrição deve ter no máximo 200 caracteres."),
     check("qtd")
-    .notEmpty()
-    .withMessage("A quantidade é obrigatória.")
-    .isInt({ min: 0, max: 999})
-    .withMessage("A quantidade deve ser um número inteiro entre 0 e 999."),
+        .notEmpty()
+        .withMessage("A quantidade é obrigatória.")
+        .isInt({ min: 0, max: 999 })
+        .withMessage("A quantidade deve ser um número inteiro entre 0 e 999."),
 
     validateRequest
 ];
@@ -47,17 +59,29 @@ export const validateStorageUpdate = [
         .isLength({ max: 10 })
         .withMessage("O código do produto deve ter no máximo 10 caracteres.")
         .matches(/^[A-Z]{2}-[A-Z]{3}-\d{3}$/)
-        .withMessage("Formato inválido. Use o padrão: AA-AAA-001."),
+        .withMessage("Formato inválido. Use o padrão: AA-AAA-001.")
+        .custom(async (cod_prod, { req }) => {
+            const db = req.app.locals.db;
+            const query = { cod_prod };
+            if (req.method === "PUT" && req.params.id) {
+                query["_id"] = { $ne: new ObjectId(req.params.id) };s
+            }
+            const existe = await db.collection("storage").countDocuments(query);
+            if (existe > 0) {
+                throw new Error("O produto informado ja está cadastrado no estoque");
+            }
+            return true;
+        }),
     check("descricao")
         .notEmpty()
         .withMessage("A descrição é obrigatória.")
         .isLength({ max: 200 })
         .withMessage("A descrição deve ter no máximo 200 caracteres."),
     check("qtd")
-    .notEmpty()
-    .withMessage("A quantidade é obrigatória.")
-    .isInt({ min: 0, max: 999})
-    .withMessage("A quantidade deve ser um número inteiro entre 0 e 999."),
+        .notEmpty()
+        .withMessage("A quantidade é obrigatória.")
+        .isInt({ min: 0, max: 999 })
+        .withMessage("A quantidade deve ser um número inteiro entre 0 e 999."),
 
     validateRequest
 ];
